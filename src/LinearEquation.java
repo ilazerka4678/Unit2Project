05d1fae764ca092dd06524e8935ca4211c0328a3
BT_4N2_05 <ilazerka4678@bths.edu>
2022-10-11 12:15:37 -0400

    Initial commit

---
+++ src/LinearEquation.java
@@ -0,0 +1,16 @@
+public class LinearEquation {
+    private int firstX;
+    private int firstY;
+    private int secondX;
+    private int secondY;
+
+    public LinearEquation(int firstX, int firstY, int secondX, int secondY){
+        this.firstX = firstX;
+        this.firstY = firstY;
+        this.secondX = secondX;
+        this.secondY = secondY;
+    }
+    public String getSlope(int firstX, int firstY, int secondX, int secondY){
+        
+    }
+}
