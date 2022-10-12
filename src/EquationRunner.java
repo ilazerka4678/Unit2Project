62b9570117fd1ae063f015b8bb098cbad12b7bfd
BT_4N2_05 <ilazerka4678@bths.edu>
2022-10-12 12:27:55 -0400

    added things

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class EquationRunner {
+    public double yIntercept;
+    public double slopeValue;
+    public int firstX;
+    public int firstY;
+    public int secondY;
+    public int secondX;
     public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the coordinates for the first point: ");
+    String firstPoint = input.nextLine();
+    int firstX = Integer.parseInt(firstPoint.substring(1,firstPoint.indexOf(",")));
+    int firstY = Integer.parseInt(firstPoint.substring(firstPoint.indexOf(",")+1, firstPoint.length()-1));
+    System.out.println(firstX);
+    System.out.println(firstY);
+    System.out.print("Enter the coordinates for the second point: ");
+    String secondPoint = input.nextLine();
+    int secondX = Integer.parseInt(secondPoint.substring(1,secondPoint.indexOf(",")));
+    int secondY = Integer.parseInt(secondPoint.substring(secondPoint.indexOf(",")+1, secondPoint.length()-1));
+    LinearEquation linEquation = new LinearEquation(firstX, firstY, secondX, secondY);
+    double slopeValue = linEquation.getSlopeDouble();
+    double yIntercept = linEquation.getIntercept();
+
 
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -4,13 +4,30 @@ public class LinearEquation {
     private int secondX;
     private int secondY;
 
-    public LinearEquation(int firstX, int firstY, int secondX, int secondY){
+    private double slope;
+
+    public LinearEquation(int firstX, int firstY, int secondX, int secondY)
+    {
         this.firstX = firstX;
         this.firstY = firstY;
         this.secondX = secondX;
         this.secondY = secondY;
     }
-    public String getSlope(int firstX, int firstY, int secondX, int secondY){
-        
+    public String getSlopeString(){
+    int numerator = secondY - firstY;
+    int denominator = secondX - firstX;
+    return numerator + "/" + denominator;
+    }
+    public double getSlopeDouble(){
+        int numerator = secondY - firstY;
+        int denominator = secondX - firstX;
+        double slope = numerator/denominator;
+        return numerator/denominator;
+    }
+    public double getIntercept(){
+    return firstY - slope*firstX;
+    }
+    public double pointDistance(){
+    return Math.sqrt(((secondX-firstX)*(secondX-firstX))-(((secondY-firstY)*(secondY-firstY))));
     }
 }
